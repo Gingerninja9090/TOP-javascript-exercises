ed0479ab80be3b3b7e8947eb55231c3577f7a398
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-04-01 15:04:23 +0200

    Excercise complete managed to create loop to do the sum, and to display error when negative however struggled to get values to swop positions, after researching found to be needed to add a const value for reference change to work
    was able to find through research the isInteger function and example
    using the logic Not ! function combined the two to create a check
    for any number not an integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR"
+    }
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR"
+    }
+    if(startNum > endNum) {
+        const newRange = startNum;
+        startNum = endNum, endNum = newRange
+    }
 
+    let sum = 0
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
