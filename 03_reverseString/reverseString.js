0e1fcaef299189174efbb5a039ee041e706068e7
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-04-01 07:53:54 +0200

    Excercise complete created a reverse string function first part splits the string into a char array second part reverse the char array last part joins the char array into a string tested and passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let wordArray = text.split("");
+    let splitWord = wordArray.reverse();
+    return splitWord.join("");
 };
 
 // Do not edit below this line
