d1887ea2f9112322219130c7dda7c5a7a902e1f2
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-04-04 13:36:16 +0200

    Excercise complete created three calculations to run and three checks based on calculation outputs passed all tests this excercise was simple and clear to achieve

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    remainderYearFours = inputYear % 4;
+    remainderYearHundreds = inputYear % 100;
+    remainderYearFourHundreds = inputYear % 400;
 
+    if (remainderYearFourHundreds === 0)
+        return true;
+    else if (remainderYearFours === 0 && remainderYearHundreds === 0)
+        return false;
+    else if (remainderYearFours === 0)
+        return true;
+    else return false;
 };
 
 // Do not edit below this line
