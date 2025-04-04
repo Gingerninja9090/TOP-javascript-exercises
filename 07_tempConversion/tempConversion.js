f4a7623653456ecce008e4a124a0c60e278e0e18
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-04-04 14:00:26 +0200

    Excercise complete simple excercise but really showed the depth of the calcualtions can go researched calculation and replicated it in the test all passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTempFah) {
+    outputTempCel = Math.round(((inputTempFah - 32) * (5 / 9)) * 10) / 10;
+    return outputTempCel
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTempCel) {
+    outputTempFah = Math.round(((inputTempCel / (5 / 9)) + 32) * 10) /10;
+    return outputTempFah
 };
 
 // Do not edit below this line
