be63c9bedaa1b4da89471eef9452e457a50e4d3b
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-05-28 10:22:15 +0200

    Excercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(reqNum) {
+    if (reqNum < 0)
+        return "OOPS"
+    if (reqNum == 0)
+        return 0;
 
+    let firstNum = 1;
+    let secondNum = 0;
+    
+    for (let i = 1; i < reqNum; i++) {
+        let current = firstNum + secondNum;
+        secondNum = firstNum;
+        firstNum = current;
+    }
+
+    return firstNum;
 };
 
 // Do not edit below this line
