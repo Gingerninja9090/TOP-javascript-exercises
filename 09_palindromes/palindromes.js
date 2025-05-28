ab3febfc2160902423142e739760c7b3d0806e5e
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-05-28 08:56:24 +0200

    Excercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const validFilter = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    filteredString = string.toLowerCase().split('').filter((values) => validFilter.includes(values)).join('');
+
+    ReversedString = filteredString.split('').reverse().join('');
+
+    return ReversedString === filteredString;
 };
 
 // Do not edit below this line
