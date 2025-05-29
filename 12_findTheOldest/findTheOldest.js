155816ff333af04a8bb252230a9070bc43d47dec
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-05-29 10:22:46 +0200

    Excercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    if (person.yearOfDeath == null) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
 
+    return person.yearOfDeath - person.yearOfBirth;
+    
 };
 
+const findTheOldest = function(people) {
+    const sortedAges = people.toSorted(
+        (person, nextPerson) => getAge(nextPerson) - getAge(person),
+    );
+
+    const oldestPerson = sortedAges[0];
+    return oldestPerson
+};
 // Do not edit below this line
 module.exports = findTheOldest;
